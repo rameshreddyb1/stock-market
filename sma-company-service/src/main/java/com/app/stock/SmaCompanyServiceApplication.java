e6cbccf2d957e0bad97c78eaf2238f9128e1b7ce
rameshreddyb1 <119746230+rameshreddyb1@users.noreply.github.com>
2022-12-05 09:19:12 +0530

    Initial commit

---
+++ sma-company-service/src/main/java/com/app/stock/SmaCompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaCompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmaCompanyServiceApplication.class, args);
+	}
+
+}
---
+++ sma-company-service/src/test/java/com/app/stock/SmaCompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaCompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
